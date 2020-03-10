225a78e3303eb67eb8e3febc9ac6250428b5f465
michae31201 <michae31201@gmail.com>
2020-03-11 03:29:54 +0800

    Updates

---
+++ precache-manifest.7884ff493eaed4ce74c8f5fa61e25956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84cdea0033c66ec8daf1ed672d177cc",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "2c11750d05ba0bac5c66",
+    "url": "/TodoList/static/css/main.920dc2f7.chunk.css"
+  },
+  {
+    "revision": "3028bb92384f959a3547",
+    "url": "/TodoList/static/js/2.8dd43f74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TodoList/static/js/2.8dd43f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c11750d05ba0bac5c66",
+    "url": "/TodoList/static/js/main.614d58c7.chunk.js"
+  },
+  {
+    "revision": "686e813702bf54971449",
+    "url": "/TodoList/static/js/runtime-main.696b0418.js"
+  }
+]);
